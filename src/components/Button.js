32770fc7a1f92b712f99824ddac5ad42f7339cfd
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-10-31 10:05:27 +0000

    Create Button Component

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import styled, { css } from 'styled-components';
+
+const primary = css`
+  background-color: ${(props) => props.theme.primary};
+`;
+
+const secondary = css`
+  background-color: ${(props) => props.theme.secondary};
+`;
+
+const Button = styled.button`
+  ${(props) => props.primary && primary};
+  ${(props) => props.secondary && secondary};
+  padding: 0 1rem;
+  border: none;
+  border-radius: ${(props) => props.theme.borderRadius};
+  font-family: 'Poppins', sans-serif;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.1) 0 0);
+  }
+`;
+
+export default Button;
