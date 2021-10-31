d1eb878f1acddf465939dde44e5af36a93e0f40a
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-10-31 16:08:28 +0000

    Add more levels to data.js

--- src/data.js
+++ src/data.js
@@ -1,32 +1,126 @@
 import level1 from './assets/images/level1.jpg';
+import level2 from './assets/images/level2.jpg';
+import level3 from './assets/images/level3.jpg';
+import level4 from './assets/images/level4.jpg';
+import level5 from './assets/images/level5.jpg';
+import level6 from './assets/images/level6.jpg';
 import waldoAvatar from './assets/images/waldo-face.png';
 import odlawAvatar from './assets/images/odlaw-face.jpg';
 import wendaAvatar from './assets/images/wenda-face.jpg';
 import wizardAvatar from './assets/images/wizard-face.jpg';
 
-export const levelOne = {
-  level: 1,
-  picture: level1,
-  characters: [
-    {
-      name: 'Waldo',
-      coordinates: null,
-      avatar: waldoAvatar,
-    },
-    {
-      name: 'Wizard',
-      coordinates: null,
-      avatar: wizardAvatar,
-    },
-    {
-      name: 'Odlaw',
-      coordinates: null,
-      avatar: odlawAvatar,
-    },
-    {
-      name: 'Wenda',
-      coordinates: null,
-      avatar: wendaAvatar,
-    },
-  ],
-};
+const data = [
+  {
+    level: 1,
+    image: level1,
+    characters: [
+      {
+        name: 'Waldo',
+        coordinates: null,
+        avatar: waldoAvatar,
+      },
+      {
+        name: 'Wizard',
+        coordinates: null,
+        avatar: wizardAvatar,
+      },
+      {
+        name: 'Odlaw',
+        coordinates: null,
+        avatar: odlawAvatar,
+      },
+    ],
+  },
+  {
+    level: 2,
+    image: level2,
+    characters: [
+      {
+        name: 'Waldo',
+        coordinates: null,
+        avatar: waldoAvatar,
+      },
+    ],
+  },
+  {
+    level: 3,
+    image: level3,
+    characters: [
+      {
+        name: 'Waldo',
+        coordinates: null,
+        avatar: waldoAvatar,
+      },
+      {
+        name: 'Wizard',
+        coordinates: null,
+        avatar: wizardAvatar,
+      },
+      {
+        name: 'Odlaw',
+        coordinates: null,
+        avatar: odlawAvatar,
+      },
+      {
+        name: 'Wenda',
+        coordinates: null,
+        avatar: wendaAvatar,
+      },
+    ],
+  },
+  {
+    level: 4,
+    image: level4,
+    characters: [
+      {
+        name: 'Waldo',
+        coordinates: null,
+        avatar: waldoAvatar,
+      },
+      {
+        name: 'Odlaw',
+        coordinates: null,
+        avatar: odlawAvatar,
+      },
+    ],
+  },
+  {
+    level: 5,
+    image: level5,
+    characters: [
+      {
+        name: 'Waldo',
+        coordinates: null,
+        avatar: waldoAvatar,
+      },
+      {
+        name: 'Wizard',
+        coordinates: null,
+        avatar: wizardAvatar,
+      },
+      {
+        name: 'Odlaw',
+        coordinates: null,
+        avatar: odlawAvatar,
+      },
+      {
+        name: 'Wenda',
+        coordinates: null,
+        avatar: wendaAvatar,
+      },
+    ],
+  },
+  {
+    level: 6,
+    image: level6,
+    characters: [
+      {
+        name: 'Waldo',
+        coordinates: null,
+        avatar: waldoAvatar,
+      },
+    ],
+  },
+];
+
+export default data;
