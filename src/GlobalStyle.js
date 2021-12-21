30ebcb3652873e5d5a6ece5a918759e268c77e66
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-12-21 15:31:01 +0000

    Add styling to scroll bar

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,6 +12,25 @@ html, body {
     -webkit-tap-highlight-color:  rgba(255, 255, 255, 0); 
 }
 
+// Scrollbar Styles
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+  border-radius: ${(props) => props.theme.borderRadius};
+  background-color: #F5F5F5;
+}
+
+::-webkit-scrollbar {
+  width: 0.8rem;
+  height: 0.8rem;
+  background-color: #F5F5F5;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: ${(props) => props.theme.borderRadius};
+  box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+  background-color: ${(props) => props.theme.primary};
+}
+
 `;
 
 export default GlobalStyle;
